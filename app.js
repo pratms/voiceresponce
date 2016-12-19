1ff65782e19b6741a338feb1e5c36e512d8bb9c6
pratms <pratikmodak@icloud.com>
2016-12-19 02:05:19 -0500

    chart

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 
 
 // view engine setup
-var db = mongojs('mongodb://pratik:pratik@ds133438.mlab.com:33438/heroku_9rvcpdq9', ['users']);
+var db = mongojs('mongodb://pratik:pratik@ds133438.mlab.com:33438/heroku_9rvcpdq9', ['users','response']);
 
 app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
@@ -28,6 +28,25 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.get('/results', function(req, res)
+{
+   db.response.find().sort({response: -1},function(err, data) 
+  {
+        if (err) {
+        throw err;
+    } 
+    else{
+
+     console.log(data);
+     res.json(data);
+
+    }
+
+  });
+
+
+});
+
 app.post('/voice', (request, response) => 
 {
   var twiml = new twilio.TwimlResponse();
@@ -53,7 +72,7 @@ app.post('/gather', (request, response) => {
                    
     {
      
-  
+  var name = data.name;
   if (data) {
     twiml.say('wellcome,'+data.name);
 
@@ -63,7 +82,7 @@ app.post('/gather', (request, response) => {
     
   }, 
   (gatherNode) => {
-    gatherNode.say('press 1 for course information, press 2 for grades.');
+    gatherNode.say('press 1 for course information,press 2 for grades,press 3 to take part in survey,press 5 to talk to our executive');
   });
   twiml.redirect('/voice');
  
@@ -84,20 +103,58 @@ app.post('/gather1', (request, response) => {
 {
       switch (request.body.Digits) {
       case '1': 
-         twiml.say('Hi there!Please speak your course number,for example cs 641 after the beep -Get ready!')
+         twiml.say('Hi there!'+name' Please speak your course number,for example cs 641 after the beep,-Get ready!')
         .record({
-            transcribe: true,
+            transcribe:true,
+            timeout:5,
             maxLength:30,
-            action:'/recording'
-        });
+            transcribeCallback:'/transcribe', 
+             action:'/transcrib'
+           
+           
+        })
 
+  
        break;
       case '2': twiml.say('wellcome to course grades!'); break;
-      default: 
+    
+      case '3':  twiml.say('Rate between 1 to 5, one being the lowest and five as highest').pause();
+       
+
+              twiml.gather({
+              method:"post",
+              action:"/survey",
+              numDigits:1
+              },
+              (gatherNode) => {
+              gatherNode.say('How effective was the teaching in your major at this university?');
+              });
+          
+         
+       break;
+        
+     
+      case '5': 
+    var twiml = new twilio.TwimlResponse();
+    twiml
+      .say('You\'ll be connected shortly connected.',
+           { voice: 'alice', language: 'en-GB' })
+      .dial({ record:"true"}, function() {
+        this.number('201-920-3362', {
+         
+        });
+        this.number('551-689-7695', {
+         
+        });
+      
+      });
+    break;
+     default: 
         twiml.say('Sorry, I don\'t understand that choice.').pause();
         twiml.redirect('/voice');
         break;
     }
+
 
 
 
@@ -110,37 +167,38 @@ app.post('/gather1', (request, response) => {
   // Render the response as XML in reply to the webhook request
   response.type('text/xml');
   response.send(twiml.toString());
+
       });
 
-
-app.post('/recording', (request,response){
+app.post('/survey', (request,response) => {
 var twiml = new twilio.TwimlResponse();
-var text = request.body.TranscriptionText;
-twiml.say('wellcome to course'+text);
-// if(request.body.Digits)
-// {
-//       switch (request.body.Digits) 
-//       {
-//       case '2': twiml.say('wellcome to course grades!'); break;
-//       default: 
-//         twiml.say('Sorry, I don\'t understand that choice.').pause();
-//         twiml.redirect('/voice');
-//         break;
-//     }
+twiml.say('The response is'+request.body.Digits);
+db.response.insert( { response: request.body.Digits  } )
 
+});
 
+app.post('/transcribe', (request,response) => {
+var twiml = new twilio.TwimlResponse();
+//   var accountSid = 'AC5b3a64ad844dfbb918812897bcf2a1ce'; 
+//     var authToken = '8c055fe15f07533ff69388be72b93b16';  
+// var client = require('twilio')(accountSid, authToken);
 
-// } 
-
-//    else {
-//     twiml.redirect('/voice');
-//   }
-
-  // Render the response as XML in reply to the webhook request
+//   client.transcriptions.list(function(err, data) {
+//     data.transcriptions.slice(0,1).forEach(function(transcription) {
+//         twiml.say(transcription.transcriptionText);
+        
+      var transcript = request.body.TranscriptionText;
+      twiml.say('hello'+transcript);
+      //a b c
+  
+//     });
+// });
   response.type('text/xml');
   response.send(twiml.toString());
 
 });
+
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
