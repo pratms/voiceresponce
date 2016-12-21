c1ad638102a4df8d850986466020aea6475b2040
pratms <pratikmodak@icloud.com>
2016-12-21 03:23:52 -0500

    survey

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 
 
 // view engine setup
-var db = mongojs('mongodb://pratik:pratik@ds133438.mlab.com:33438/heroku_9rvcpdq9', ['users','response']);
+var db = mongojs('mongodb://pratik:pratik@ds133438.mlab.com:33438/heroku_9rvcpdq9', ['users','response','news']);
 
 app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
@@ -30,14 +30,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.get('/results', function(req, res)
 {
-   db.response.find().sort({response: -1},function(err, data) 
+   db.response.find({queid:"1"}).sort({response: -1},function(err, data) 
   {
         if (err) {
         throw err;
     } 
     else{
 
-     console.log(data);
      res.json(data);
 
     }
@@ -46,6 +45,29 @@ app.get('/results', function(req, res)
 
 
 });
+app.get('/result2', function(req, res)
+{
+   db.response.find({ queid: "2"}).sort({response: -1},function(err, data) 
+  {
+        if (err) {
+        throw err;
+    } 
+    else{
+
+     
+     res.json(data);
+   
+
+    }
+
+  });
+   
+
+
+
+
+});
+
 
 app.post('/voice', (request, response) => 
 {
@@ -68,7 +90,15 @@ app.post('/voice', (request, response) =>
 app.post('/gather', (request, response) => {
   // Use the Twilio Node.js SDK to build an XML response
   var twiml = new twilio.TwimlResponse();
-   db.users.findOne({userid: request.body.Digits }, function(err, data) 
+  if(request.param('userid'))
+  {
+  var uid = request.param('userid');
+  }
+  else
+  {
+    var uid = request.body.Digits;
+  }
+   db.users.findOne({userid: uid }, function(err, data) 
                    
     {
      
@@ -78,11 +108,11 @@ app.post('/gather', (request, response) => {
 
     twiml.gather({ 
     numDigits: 1,
-    action: '/gather1'
+    action: '/gather1?userid='+data.userid
     
   }, 
   (gatherNode) => {
-    gatherNode.say('press 1 for course information,press 2 for grades,press 3 to take part in survey,press 5 to talk to our executive');
+    gatherNode.say('press 1 for course information, press 2 for grades, press 3 to take part in survey, press 4 for news and updates, press 5 to talk to our executive');
   });
   twiml.redirect('/voice');
  
@@ -99,100 +129,262 @@ app.post('/gather', (request, response) => {
 app.post('/gather1', (request, response) => {
   // Use the Twilio Node.js SDK to build an XML response
   var twiml = new twilio.TwimlResponse();
+  var id = request.param('userid');
+
+
+
+
   if(request.body.Digits)
-{
-      switch (request.body.Digits) {
+  {
+
+    switch (request.body.Digits) {
       case '1': 
-         twiml.say('Hi there!'+name' Please speak your course number,for example cs 641 after the beep,-Get ready!')
+      twiml.say('Hi there! Please speak your course number,for example cs 641 after the beep,-Get ready!')
         .record({
-            transcribe:true,
-            timeout:5,
-            maxLength:30,
-            transcribeCallback:'/transcribe', 
-             action:'/transcrib'
-           
-           
+      transcribe:true,
+      timeout:5,
+      maxLength:30,
+      transcribeCallback:'/transcribe',
+      action:'/recording'
+
+
         })
 
-  
-       break;
-      case '2': twiml.say('wellcome to course grades!'); break;
-    
-      case '3':  twiml.say('Rate between 1 to 5, one being the lowest and five as highest').pause();
-       
 
-              twiml.gather({
-              method:"post",
-              action:"/survey",
-              numDigits:1
-              },
-              (gatherNode) => {
-              gatherNode.say('How effective was the teaching in your major at this university?');
-              });
-          
-         
-       break;
-        
-     
-      case '5': 
-    var twiml = new twilio.TwimlResponse();
-    twiml
-      .say('You\'ll be connected shortly connected.',
-           { voice: 'alice', language: 'en-GB' })
-      .dial({ record:"true"}, function() {
-        this.number('201-920-3362', {
-         
-        });
-        this.number('551-689-7695', {
-         
-        });
-      
-      });
-    break;
-     default: 
-        twiml.say('Sorry, I don\'t understand that choice.').pause();
-        twiml.redirect('/voice');
-        break;
-    }
+break;
+                
+case '2':twiml.say('wellcome to couse grades!');
+twiml.redirect('/grades?userid='+id);
 
+
+
+break;
+
+
+case '3':  twiml.say('Rate between 1 to 5, one being the lowest and five as highest, lets begin').pause();
+
+
+twiml.gather({
+  method:"post",
+  numDigits:1,
+  action:"/survey"
+
+},
+(gatherNode) => {
+  gatherNode.say('How effective was the teaching in your major at this university?');
+});
+
+
+break;
+case '4':  twiml.say('wellcome to daily news updates').pause();
+
+twiml.redirect('/news?userid='+id);
+break;
+
+
+case '5': 
+var twiml = new twilio.TwimlResponse();
+twiml
+.say('You\'ll be connected shortly connected.',
+ { voice: 'alice', language: 'en-GB' })
+.dial({ record:"true"}, function() {
+  this.number('201-920-3362', {
+
+  });
+  this.number('551-263-6012', {
+
+  });
+   this.number('201-920-4433', {
+
+  });
+
+});
+break;
+default: 
+twiml.say('Sorry, I don\'t understand that choice.').pause();
+twiml.redirect('/voice');
+break;
+}
 
 
 
 } 
 
-   else {
-    twiml.redirect('/voice');
-  }
+else {
+  twiml.redirect('/voice');
+}
 
   // Render the response as XML in reply to the webhook request
   response.type('text/xml');
   response.send(twiml.toString());
+});
 
-      });
+app.post('/grades', (request,response) => {
+var twiml = new twilio.TwimlResponse();
 
+db.users.findOne({userid: request.param('userid')}, function(err, data) 
+  {
+  
+  twiml.say("Your previous semester grades!").pause();
+  twiml.say("1 "+data.prevcourses[0]).pause();
+  twiml.say("your grade "+data.grades[0]).pause();
+  twiml.say("2 "+data.prevcourses[1]).pause();
+  twiml.say("your grade "+data.grades[1]).pause();
+  twiml.say("3 "+data.prevcourses[2]).pause();
+  twiml.say("your grade "+data.grades[2]).pause(
+    {
+      length:2
+}
+    );
+  twiml.gather({
+  timeout:5,
+  method:"post",
+  numDigits:1,
+  action:"/gather?userid="+request.param('userid')
+
+},
+(gatherNode) => {
+  gatherNode.say('Press 9 to go back to previous menu');
+});
+    response.type('text/xml');
+  response.send(twiml.toString());
+
+                });
+
+  
+});
 app.post('/survey', (request,response) => {
 var twiml = new twilio.TwimlResponse();
-twiml.say('The response is'+request.body.Digits);
-db.response.insert( { response: request.body.Digits  } )
+if(request.body.Digit!=0 && request.body.Digits <= 5 ){   
+twiml.say('Question 2').hangup();  
+
+   
+db.response.insert( { response: request.body.Digits, queid: "1"  } );
+
+}
+else{
+   twiml.say('Invalid response try again!').hangup(); 
+   
+}
+
+
+twiml.gather({
+  method:"post",
+  numDigits:1,
+  action:"/survey2"
+
+},
+(gatherNode) => {
+  gatherNode.say('Are you satisfied with the quality of food served at this university? press 1 if satisfied, press 2 if not');
+});
+   
+  response.type('text/xml');
+  response.send(twiml.toString());
+   
+});
+
+app.post('/survey2', (request,response) => {
+var twiml = new twilio.TwimlResponse();
+if(request.body.Digit!=0 && request.body.Digits <= 5 ){   
+twiml.say('Thank you for your response good bye!').hangup();  
+var accountSid = 'AC5b3a64ad844dfbb918812897bcf2a1ce'; 
+var authToken = '8c055fe15f07533ff69388be72b93b16';  
+var client = new twilio.RestClient(accountSid, authToken);
+
+   
+db.response.insert( { response: request.body.Digits, queid: "2"  } );
+
+   
+client.messages.create({
+    body: 'Thanks for participating in survey, for survey result visit https://voiceresponse.herokuapp.com/',
+    to: request.body.From,  
+    from: '+16466528019' 
+}, function(err) {
+    console.log(err);
+}
+);
+}
+else{
+   twiml.say('Invalid response try again!').hangup(); 
+   
+}
+
+   
+  response.type('text/xml');
+  response.send(twiml.toString());
+   
+});
+//survey question 2
+
+app.post('/news', (request,response) => {
+var twiml = new twilio.TwimlResponse();
+
+db.news.findOne(function(err, data) 
+  {
+  
+  twiml.say(data.news);
+  twiml.gather({
+  timeout:5,
+  method:"post",
+  numDigits:1,
+  action:"/gather?userid="+request.param('userid')
+
+},
+(gatherNode) => {
+  gatherNode.say('Press 9 to go back to previous menu');
+});
+    response.type('text/xml');
+  response.send(twiml.toString());
+
+  });
 
 });
 
-app.post('/transcribe', (request,response) => {
-var twiml = new twilio.TwimlResponse();
-//   var accountSid = 'AC5b3a64ad844dfbb918812897bcf2a1ce'; 
-//     var authToken = '8c055fe15f07533ff69388be72b93b16';  
-// var client = require('twilio')(accountSid, authToken);
-
-//   client.transcriptions.list(function(err, data) {
-//     data.transcriptions.slice(0,1).forEach(function(transcription) {
-//         twiml.say(transcription.transcriptionText);
-        
-      var transcript = request.body.TranscriptionText;
-      twiml.say('hello'+transcript);
-      //a b c
+app.post('/recording', (request,response) => {
+  var twiml = new twilio.TwimlResponse();
+  // A message for the user
+   for(var i = 0; i<= 3; i++) {
+        twiml.say('Please wait while we transcribe your answer.');
+  twiml.pause(
+     {
+     length:"2"
+     }
   
-//     });
-// });
+  );
+  // Then redirect around again while we wait
+ 
+}
+     twiml.redirect('/transcribe');
+
+  response.type('text/xml');
+  response.send(twiml.toString());
+});
+app.post('/transcribe', (request, response) => {
+  var text = request.body.TranscriptionText;
+  var callSid = request.body.CallSid;
+
+  // Do something with the text
+  var courseId =text;
+
+  var accountSid = 'AC5b3a64ad844dfbb918812897bcf2a1ce'; 
+    var authToken = '8c055fe15f07533ff69388be72b93b16';  
+var client = require('twilio')(accountSid, authToken);
+
+  // Redirect the call
+  client.calls(callSid).update({
+    url: '/course?courseId=' + courseId,
+    method: 'POST'
+  }, (err, res) => {
+    response.sendStatus(200);
+  })
+});
+
+app.post('/course', (request,response) => {
+var twiml = new twilio.TwimlResponse();
+    var courseId = request.param('courseId');
+
+       
+      twiml.say('hello'+courseId);
   response.type('text/xml');
   response.send(twiml.toString());
 
--- public/javascripts/angular.js
+++ public/javascripts/angular.js
@@ -1,121 +1,121 @@
 
-				  //  var myApp = angular.module("myApp" , [])
-				  //  .controller("myController" , function($scope, $http)
-				   	
-				  //  {
-				  
-
-						// $scope.SendData = function() {
-						// $http.post('/view1',$scope.formData).
-						// success(function(data) {
-						// $scope.success = "Message Sent";
-						// }).error(function(data) {
-						// console.error("error in posting");
-						// })
-						// }
-
-						// $scope.ReceiveData = function(){
-
-						// $http.get("/posts").success(function (response)
-						// {
-							
-						// 	$scope.show = true;
-
-
-						// $scope.data = response;
-
-
-
-						// });
-
-
-
-
-						// }
-					
-
-						// $http.get("/").success(function (response)
-						// {
-
-
-						// $scope.data = response;
-
-
-
-						// });
-
-		   
-				  //  });
 var app = angular.module('myApp', ['chart.js']); 
-    
+
 app.controller('DoughnutCtrl', function ($scope, $http) {
-    $scope.labels = [];
-$scope.data=[];
-    
- $http.get("/results").success(function (response)
-						{
-							console.log(response);
-						$scope.zdata= response.sort();
-						console.log($scope.zdata.sort());
-						var datalength = $scope.zdata.length;
-						
-						
-						var current = null;
-						var cnt = 0;
+	$scope.labels = [];
+	$scope.data=[];    
+	$http.get("/results").success(function (response)
+	{
+	
+		$scope.zdata= response;
 
-						for (var i = 0; i < datalength; i++) {
-							if ($scope.zdata[i].response != current) {
-								if (cnt > 0) {
-									var label = "Rated"+current;
-									$scope.labels.push(label);
-									$scope.data.push(cnt);
-									
-								}
-								current = $scope.zdata[i].response;
-								cnt = 1;
-
-							} else 
-							{
-								cnt++;
-								
-
-							}
+			var datalength = $scope.zdata.length;
 
 
-						}
-						if (cnt > 0) {
-							label = "Rated"+current;
-							$scope.labels.push(label);
-							$scope.data.push(cnt);
-							
-						}
-						console.log($scope.data);
-						
-						});
+			var current = null;
+			var cnt = 0;
+
+			for (var i = 0; i < datalength; i++) {
+				if ($scope.zdata[i].response != current) {
+					if (cnt > 0) {
+						var label = "Rated"+current;
+						$scope.labels.push(label);
+						$scope.data.push(cnt);
+
+					}
+					current = $scope.zdata[i].response;
+					cnt = 1;
+
+				} else 
+				{
+					cnt++;
 
 
-    $scope.Color= ['#90EE90', '#FF6600', '#8080FF'];
+				}
+
+
+			}
+			if (cnt > 0) {
+				label = "Rated"+current;
+				$scope.labels.push(label);
+				$scope.data.push(cnt);
+
+			}
+			
+
+		});
+
+
+    $scope.Color= ['#90EE90', '#FF6600','#4D5360'];
     //PieDataSetOverride is used to draw lines to display the labels
 
     $scope.DataSetOverride = [{ yAxisID: 'y-axis-1' }]; //y-axis-1 is the ID defined in scales under options.
 
     $scope.options = {
-        legend: { display: true },
+    	legend: { display: true },
         responsive: true,  // set to false to remove responsiveness. Default responsive value is true.
-     animation:{
-        animateRotate:true,
-        animateScale:true,
-    },
-    cutoutPercentage:70,
+        animation:{
+        	animateRotate:true,
+        	animateScale:true,
+        },
+        cutoutPercentage:70,
 
         scales: {
-            yAxes: [
-                {
-                    id: 'y-axis-1',
-                    type: 'linear',
-                    display: true,
-                    position: 'left'
-                }]
+        	yAxes: [
+        	{
+        		id: 'y-axis-1',
+        		type: 'linear',
+        		display: true,
+        		position: 'left'
+        	}]
         }
     }
+
+});
+app.controller("BaseCtrl",
+  function ($scope, $http) {
+    $scope.labels = ['No','Yes'];
+    $scope.data = [];
+    $scope.type = 'polarArea';
+    $http.get("/result2").success(function (response)
+	{
+		
+		$scope.zdata= response;
+		
+			var datalength = $scope.zdata.length;
+
+
+			var current = null;
+			var cnt = 0;
+
+			for (var i = 0; i < datalength; i++) {
+				if ($scope.zdata[i].response != current) {
+					if (cnt > 0) {
+					
+						$scope.data.push(cnt);
+
+					}
+					current = $scope.zdata[i].response;
+					cnt = 1;
+
+				} else 
+				{
+					cnt++;
+
+
+				}
+
+
+			}
+			if (cnt > 0) {
+	
+				$scope.data.push(cnt);
+
+			}
+		
+});
+    $scope.toggle = function () {
+      $scope.type = $scope.type === 'polarArea' ?
+        'pie' : 'polarArea';
+    };
 });
